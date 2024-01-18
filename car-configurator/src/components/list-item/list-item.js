afb1651d8ee2e06f76043e927a86a713d09713f7
Vova <vovasanduliak@gmail.com>
2024-01-18 21:06:43 +0200

    # Add item list

--- car-configurator/src/components/list-item/list-item.js
+++ car-configurator/src/components/list-item/list-item.js
@@ -1,18 +1,27 @@
-import bmw from "./bmw-m8-competition-gran-coupe.png"
+import bmw_image from "./bmw-m8-competition-gran-coupe.png";
+import "./list-item.css";
 
 function CarItem() {
   return (
-    <div className="car-item">
-      <img
-        className="car-image"
-        src={bmw}
-        alt="car"
-      />
-      <div className="car-details">
-        <h3 className="car-title">BMW M8</h3>
-        <p className="car-description">From 100000$</p>
+    <div className="product-card">
+      <div className="product-image">
+        <img src={bmw_image} alt="" />
+      </div>
+      <div className="product-details">
+        <h2 className="product-title">BMW M8</h2>
+        <h4 className="product-price"> from 100.000$</h4>
       </div>
     </div>
+    // < className="car-item">
+    //   <img
+    //     className="car-image"
+    //     src={bmw}
+    //     alt="car"
+    //   />
+    //   <div className="car-details">
+    //     <h3 className="car-title">BMW M8</h3>
+    //     <p className="car-description">From 100000$</p>
+    //   </div>
   );
 }
 
