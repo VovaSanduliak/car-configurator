c0def77adc8d11ae980b2626913b3d937cc77fd2
VovaSanduliak <vovasanduliak@gmail.com>
2024-01-22 21:48:30 +0200

    # Added Contact form component (without css)

--- car-configurator/src/components/contact/contact.js
+++ car-configurator/src/components/contact/contact.js
@@ -1,5 +1,69 @@
+import { useState } from "react";
+
 const Contact = () => {
-  return <h1>Contact</h1>;
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Last name
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Subject
+          <input
+            type="text"
+            name="subject"
+            value={formData.subject}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Message
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      <div>
+        <button type="submit">Send</button>
+      </div>
+    </form>
+  );
 };
 
 export default Contact;
