de51265fef4467050d80f15cc9e162a646159c2b
VovaSanduliak <vovasanduliak@gmail.com>
2024-01-25 18:27:49 +0200

    add responsive navbar (preview)

--- car-configurator/src/components/header/header.js
+++ car-configurator/src/components/header/header.js
@@ -2,13 +2,42 @@ import Nav from "../nav/nav";
 import Logo from "../logo/logo";
 
 import "./header.css";
+import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState();
+
   return (
-    <header style={{}}>
-      <Logo />
-      <Nav />
-    </header>
+    <nav>
+      <Link to="/" className="title">
+        Logo
+      </Link>
+      <div className="menu" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={isMenuOpen ? "open" : ""}>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/models">Models</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+    </nav>
+
+    // <header style={{}}>
+    //   <Logo />
+    //   <Nav />
+    // </header>
   );
 };
 
